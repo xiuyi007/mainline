db3b3de209ae780cca8ea7088d6eeaa6c5f55b0a
togethf <1720425326@qq.com>
2024-08-01 18:51:54 +0800

    add MLCA Att

--- modules/attention.py
+++ modules/attention.py
@@ -13,7 +13,8 @@ import torch.utils.checkpoint as checkpoint
 from typing import List
 from timm.models.layers import DropPath
 from efficientnet_pytorch.model import MemoryEfficientSwish
-
+import math
+from ultralytics.nn.modules import C3, Bottleneck, C2f
 
 __all__ = (
     "CBAM",
@@ -37,7 +38,9 @@ __all__ = (
     "MSDABlock",
     "C2f_MSDA",
     "DeformableAttention2D",
-    "CloFormerEfficientAttention"
+    "CloFormerEfficientAttention",
+    "MLCA",
+    "C2f_MLCA"
 )
 
 class ChannelAttention(nn.Module):
@@ -1366,3 +1369,74 @@ class CloFormerEfficientAttention(nn.Module):
         if self.group_split[-1] != 0:
             res.append(self.low_fre_attention(x, self.global_q, self.global_kv, self.avgpool))
         return self.proj_drop(self.proj(torch.cat(res, dim=1)))
+
+
+class MLCA(nn.Module):
+    def __init__(self, in_size, local_size=5, gamma=2, b=1, local_weight=0.5):
+        super(MLCA, self).__init__()
+
+        # ECA 计算方法
+        self.local_size = local_size
+        self.gamma = gamma
+        self.b = b
+        t = int(abs(math.log(in_size, 2) + self.b) / self.gamma)  # eca  gamma=2
+        k = t if t % 2 else t + 1
+
+        self.conv = nn.Conv1d(1, 1, kernel_size=k, padding=(k - 1) // 2, bias=False)
+        self.conv_local = nn.Conv1d(1, 1, kernel_size=k, padding=(k - 1) // 2, bias=False)
+
+        self.local_weight = local_weight
+
+        self.local_arv_pool = nn.AdaptiveAvgPool2d(local_size)
+        self.global_arv_pool = nn.AdaptiveAvgPool2d(1)
+
+    def forward(self, x):
+        local_arv = self.local_arv_pool(x)
+        global_arv = self.global_arv_pool(local_arv)
+
+        b, c, m, n = x.shape
+        b_local, c_local, m_local, n_local = local_arv.shape
+
+        # (b,c,local_size,local_size) -> (b,c,local_size*local_size)-> (b,local_size*local_size,c)-> (b,1,local_size*local_size*c)
+        temp_local = local_arv.view(b, c_local, -1).transpose(-1, -2).reshape(b, 1, -1)
+        temp_global = global_arv.view(b, c, -1).transpose(-1, -2)
+
+        y_local = self.conv_local(temp_local)
+        y_global = self.conv(temp_global)
+
+        # (b,c,local_size,local_size) <- (b,c,local_size*local_size)<-(b,local_size*local_size,c) <- (b,1,local_size*local_size*c)
+        y_local_transpose = y_local.reshape(b, self.local_size * self.local_size, c).transpose(-1, -2).view(b, c,
+                                                                                                            self.local_size,
+                                                                                                            self.local_size)
+        y_global_transpose = y_global.view(b, -1).transpose(-1, -2).unsqueeze(-1)
+
+        # 反池化
+        att_local = y_local_transpose.sigmoid()
+        att_global = F.adaptive_avg_pool2d(y_global_transpose.sigmoid(), [self.local_size, self.local_size])
+        att_all = F.adaptive_avg_pool2d(att_global * (1 - self.local_weight) + (att_local * self.local_weight), [m, n])
+
+        x = x * att_all
+        return x
+
+
+class Bottleneck_MLCA(Bottleneck):
+
+    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):  # ch_in, ch_out, shortcut, groups, kernels, expand
+        super().__init__(c1, c2, shortcut, g, k, e)
+        self.attention = MLCA(c2)
+
+    def forward(self, x):
+        return x + self.attention(self.cv2(self.cv1(x))) if self.add else self.attention(self.cv2(self.cv1(x)))
+
+
+class C3_MLCA(C3):
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        super().__init__(c1, c2, n, shortcut, g, e)
+        c_ = int(c2 * e)  # hidden channels
+        self.m = nn.Sequential(*(Bottleneck_MLCA(c_, c_, shortcut, g, k=(1, 3), e=1.0) for _ in range(n)))
+
+
+class C2f_MLCA(C2f):
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        super().__init__(c1, c2, n, shortcut, g, e)
+        self.m = nn.ModuleList(Bottleneck_MLCA(self.c, self.c, shortcut, g, k=(3, 3), e=1.0) for _ in range(n))
