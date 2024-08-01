e55cac336807d79b1a2de4c002fbc26b47b9fdb2
togethf <1720425326@qq.com>
2024-08-01 15:27:01 +0800

    add Polarized Self Att

--- modules/attention.py
+++ modules/attention.py
@@ -22,7 +22,9 @@ __all__ = (
     "DoubleAttention",
     "PSDoubleA",
     "SimAM",
-    "PSSimAM"
+    "PSSimAM",
+    "PolarizedSelfAttention",
+    "PSPSA"
 )
 
 class ChannelAttention(nn.Module):
@@ -478,3 +480,72 @@ class PSSimAM(nn.Module):
         b = b + self.ffn(b)
         return self.cv2(torch.cat((a, b), 1))
 ######################  SimAM   ####     end   by  AI&CV  ###############################
+
+###################### PolarizedSelfAttention     ####     start   by  AI&CV  ###############################
+
+class PolarizedSelfAttention(nn.Module):
+
+    def __init__(self, c1, channel=512):
+        super().__init__()
+        self.ch_wv = nn.Conv2d(channel, channel // 2, kernel_size=(1, 1))
+        self.ch_wq = nn.Conv2d(channel, 1, kernel_size=(1, 1))
+        self.softmax_channel = nn.Softmax(1)
+        self.softmax_spatial = nn.Softmax(-1)
+        self.ch_wz = nn.Conv2d(channel // 2, channel, kernel_size=(1, 1))
+        self.ln = nn.LayerNorm(channel)
+        self.sigmoid = nn.Sigmoid()
+        self.sp_wv = nn.Conv2d(channel, channel // 2, kernel_size=(1, 1))
+        self.sp_wq = nn.Conv2d(channel, channel // 2, kernel_size=(1, 1))
+        self.agp = nn.AdaptiveAvgPool2d((1, 1))
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+
+        # Channel-only Self-Attention
+        channel_wv = self.ch_wv(x)  # bs,c//2,h,w
+        channel_wq = self.ch_wq(x)  # bs,1,h,w
+        channel_wv = channel_wv.reshape(b, c // 2, -1)  # bs,c//2,h*w
+        channel_wq = channel_wq.reshape(b, -1, 1)  # bs,h*w,1
+        channel_wq = self.softmax_channel(channel_wq)
+        channel_wz = torch.matmul(channel_wv, channel_wq).unsqueeze(-1)  # bs,c//2,1,1
+        channel_weight = self.sigmoid(self.ln(self.ch_wz(channel_wz).reshape(b, c, 1).permute(0, 2, 1))).permute(0, 2,
+                                                                                                                 1).reshape(
+            b, c, 1, 1)  # bs,c,1,1
+        channel_out = channel_weight * x
+
+        # Spatial-only Self-Attention
+        spatial_wv = self.sp_wv(x)  # bs,c//2,h,w
+        spatial_wq = self.sp_wq(x)  # bs,c//2,h,w
+        spatial_wq = self.agp(spatial_wq)  # bs,c//2,1,1
+        spatial_wv = spatial_wv.reshape(b, c // 2, -1)  # bs,c//2,h*w
+        spatial_wq = spatial_wq.permute(0, 2, 3, 1).reshape(b, 1, c // 2)  # bs,1,c//2
+        spatial_wq = self.softmax_spatial(spatial_wq)
+        spatial_wz = torch.matmul(spatial_wq, spatial_wv)  # bs,1,h*w
+        spatial_weight = self.sigmoid(spatial_wz.reshape(b, 1, h, w))  # bs,1,h,w
+        spatial_out = spatial_weight * x
+        out = spatial_out + channel_out
+        return out
+
+
+class PSPSA(nn.Module):
+
+    def __init__(self, c1, c2, e=0.5):
+        super().__init__()
+        assert (c1 == c2)
+        self.c = int(c1 * e)
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv(2 * self.c, c1, 1)
+
+        self.attn = PolarizedSelfAttention(self.c, self.c)
+        self.ffn = nn.Sequential(
+            Conv(self.c, self.c * 2, 1),
+            Conv(self.c * 2, self.c, 1, act=False)
+        )
+
+    def forward(self, x):
+        a, b = self.cv1(x).split((self.c, self.c), dim=1)
+        b = b + self.attn(b)
+        b = b + self.ffn(b)
+        return self.cv2(torch.cat((a, b), 1))
+
+###################### PolarizedSelfAttention    ####     end   by  AI&CV  ###############################
