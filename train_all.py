ddd1a1b44f44eaf489aeace34f9de2ce13c0d3a1
togethf <1720425326@qq.com>
2024-08-02 21:02:27 +0800

    update train_all.py, training all the model in cfg/models

--- train_all.py
+++ train_all.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 from ultralytics import YOLO
 
@@ -8,6 +9,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='YOLO Version argument')
     parser.add_argument('--epochs', type=int, default=100, help='epochs number')
     opt = parser.parse_args()
-    for v in [8, 10]:
-        model = YOLO(f"yolov{v}s.pt")
-        model.train(data="./cfg/datasets/pest_v4.yaml", epochs=opt.epochs, imgsz=640, batch=32)
+    model_dir = 'cfg/models/'
+    i = 0
+    for dir_path, dir_name, files in os.walk(model_dir):
+        for file in files:
+            if not file.startswith('yolo'):
+                continue
+            name = os.path.join(dir_path, file)
+            model = YOLO(name).load('yolov10s.pt')
+            model.train(data="cfg/datasets/pest_v3.yaml", epochs=opt.epochs, imgsz=640, batch=32)
