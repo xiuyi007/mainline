1dd0c719d3e48e6f3677cbfcb05607e6a448b1df
togethf <1720425326@qq.com>
2024-08-06 19:33:01 +0800

    improve train.py, add some train parameter

--- train.py
+++ train.py
@@ -6,10 +6,13 @@ from ultralytics import YOLO
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='YOLO Version argument')
+    parser.add_argument('--data', type=str, default='cfg/datasets/pest_v3.yaml', help='dataset yaml')
     parser.add_argument('--model', type=str, default=None, help='yolo model')
     parser.add_argument('--epochs', type=int, default=20, help='epoch number')
+    parser.add_argument('--imgsz', type=int, default=640, help='image size')
+    parser.add_argument('--batch', type=int, default=32, help='batch size')
     # parser.print_help()
 
     opt = parser.parse_args()
     model = YOLO(opt.model).load('yolov10s.pt')
-    model.train(data="cfg/datasets/pest_v3.yaml", epochs=opt.epochs, imgsz=640, batch=32)
+    model.train(data=opt.yaml, epochs=opt.epochs, imgsz=opt.imgsz, batch=opt.batch)
